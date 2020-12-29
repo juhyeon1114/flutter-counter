64dc9cadb20336f1f34cede825776724b2f1d999
unknown <juhyeon1114@gmail.com>
2020-12-29 14:43:46 +0900

    log : 완

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,10 +28,36 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  // void addCount() {
+  //   _counter++;
+  // }
+
+  final counterSubject = BehaviorSubject<int>(); //int를 입력받고 마지막에 입력받은 것을 return함. stream을 지원함
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(child: Text('add'), onPressed: () {
+            counterSubject.add(++_counter);
+          },),
+          StreamBuilder<int>(
+            stream: counterSubject.stream,
+            initialData: 0,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text('${snapshot.data}', style: TextStyle(fontSize: 30));
+              } else {
+                return null;
+              }
+            },
+          ),
+        ],
+      )
     );
   }
 }
